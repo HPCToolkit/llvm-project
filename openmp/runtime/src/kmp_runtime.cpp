cc7b8b05398b1c636986667103837e6de67684f9
vladaindjic <vladaindjic@icloud.com>
2021-07-09 23:57:20 +0200

    No need to set exit_frame inside __kmp_serialized_parallel when the nested serialized parallel region is created.

--- openmp/runtime/src/kmp_runtime.cpp
+++ openmp/runtime/src/kmp_runtime.cpp
@@ -1315,8 +1315,6 @@ void __kmp_serialized_parallel(ident_t *loc, kmp_int32 global_tid,
   // __ompt_lw_taskteam_link function.
   if (ompt_enabled.enabled &&
       this_thr->th.ompt_thread_info.state != ompt_state_overhead) {
-    OMPT_CUR_TASK_INFO(this_thr)->frame.exit_frame.ptr =
-        OMPT_GET_FRAME_ADDRESS(0);
 
     ompt_lw_taskteam_t lw_taskteam;
     __ompt_lw_taskteam_init(&lw_taskteam, this_thr, global_tid,
