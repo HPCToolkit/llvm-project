9f0a52db936c789dbfec5be3d85781efa2f818e2
vladaindjic <vladaindjic@icloud.com>
2022-05-14 17:34:28 +0200

    GOMP - Do not call lwt link if tool is not attached.

--- openmp/runtime/src/kmp_runtime.cpp
+++ openmp/runtime/src/kmp_runtime.cpp
@@ -2084,6 +2084,7 @@ int __kmp_fork_call(ident_t *loc, int gtid,
         }
       } else if (call_context == fork_context_gnu) {
 #if OMPT_SUPPORT
+        if (ompt_enabled.enabled) {
         ompt_lw_taskteam_t lwt;
         __ompt_lw_taskteam_init(&lwt, master_th, gtid, &ompt_parallel_data,
                                 return_address);
@@ -2093,6 +2094,7 @@ int __kmp_fork_call(ident_t *loc, int gtid,
 //	OMPT_FRAME_CLEAR(task_frame, exit);
         __ompt_lw_taskteam_link(&lwt, master_th, 1);
 // don't use lw_taskteam after linking. content was swaped
+        }
 #endif
 
         // we were called from GNU native code
